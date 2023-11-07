1cac62996a1abd0c885c3f08ba8f3a252c240ecf
OgataiKhan <badellino.kevin@gmail.com>
2023-11-07 11:34:40 +0100

    fix: remove superfluous comment

--- js/main.js
+++ js/main.js
@@ -26,7 +26,7 @@ if (passengerAge < 18) {
 
 const finalPrice = discountedPrice.toFixed(2);
 
-// 6. We'll use Number.isFinite() to check whether our final result is a finite number (excluding Infinity, -Infinity, and NaN). Note that the output of toFixed() is always a string, so we need to convert it into a number again first.
+// 6. We'll use Number.isFinite() to check whether our result is a finite number (excluding Infinity, -Infinity, and NaN)
 // 7. If so, we are going to output the final price into the console and into the html document
 
 if (Number.isFinite(discountedPrice)) {
